617b630c4e5084a9cd10943dfdf48e4b2794da26
William Grzybowski <william@grzy.org>
2022-08-19 08:47:36 -0300

    Allow a second pool domain

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,4 +20,12 @@ sed -i s,%%API_HOSTNAME%%,${API_HOSTNAME:=api},g /etc/Caddyfile
 sed -i s,%%POOL_HOSTNAME%%,${POOL_HOSTNAME:=pool},g /etc/Caddyfile
 sed -i s,%%WEB_HOSTNAME%%,${WEB_HOSTNAME:=web},g /etc/Caddyfile
 
+# Allow a second pool domain e.g. pool-eu
+if [ -n ${POOL2_DOMAIN} ]; then
+	sed -i s,%%POOL2_DOMAIN%%,${POOL2_DOMAIN},g /etc/Caddyfile
+	sed -i s,%%POOL2_DOMAIN_END%%,,g /etc/Caddyfile
+else
+	sed -i s,%%POOL2_DOMAIN%%.*%%POOL2_DOMAIN_END%%,,g /etc/Caddyfile
+fi
+
 exec caddy run --config /etc/Caddyfile
