29ff33993bf57a27fa5743a5e4533b1231772d06
William Grzybowski <william@grzy.org>
2022-08-19 10:29:13 -0300

    Wrap in quotes

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,7 @@ sed -i s,%%POOL_HOSTNAME%%,${POOL_HOSTNAME:=pool},g /etc/Caddyfile
 sed -i s,%%WEB_HOSTNAME%%,${WEB_HOSTNAME:=web},g /etc/Caddyfile
 
 # Allow a second pool domain e.g. pool-eu
-if [ -n ${POOL2_DOMAIN} ]; then
+if [ -n "${POOL2_DOMAIN}" ]; then
 	sed -i s,%%POOL2_DOMAIN%%,${POOL2_DOMAIN},g /etc/Caddyfile
 	sed -i s,%%POOL2_DOMAIN_END%%,,g /etc/Caddyfile
 else
